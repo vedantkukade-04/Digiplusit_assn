2d65cebf08ad3631acb9a35b941f5dddb44b4c35
Vedant Kukade <69698832+vedantkukade-04@users.noreply.github.com>
2023-11-07 16:43:12 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+// Filename - App.js
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <table>
+        <tr>
+          <th>Name</th>
+          <th>Age</th>
+          <th>Gender</th>
+        </tr>
+        <tr>
+          <td>Anom</td>
+          <td>19</td>
+          <td>Male</td>
+        </tr>
+        <tr>
+          <td>Megha</td>
+          <td>19</td>
+          <td>Female</td>
+        </tr>
+        <tr>
+          <td>Subham</td>
+          <td>25</td>
+          <td>Male</td>
+        </tr>
+      </table>
+    </div>
+  );
+}
+
+export default App;
